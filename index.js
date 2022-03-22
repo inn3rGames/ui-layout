478f9fc6118899c28676aed215b3dc6559a03be8
inn3rGames <hi@inn3r.com>
2022-03-22 07:12:44 +0200

    Start carousel

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ window.onload = () => {
     let cards = Array.from(document.getElementsByClassName("item-card"));
 
     for (let i = 0; i < cards.length; i++) {
-
         let sparks = Array.from(cards[i].getElementsByClassName("spark"));
         let enabled = "spark on";
         let disabled = "spark off";
@@ -33,4 +32,35 @@ window.onload = () => {
             });
         }
     }
+
+    let middle = document.getElementById("middle");
+    let carousel = document.getElementById("carousel");
+    let isPointerDown = false;
+    let pointerX;
+
+    middle.addEventListener("pointerdown", (e) => {
+        isPointerDown = true;
+        pointerX = e.offsetX - carousel.offsetLeft;
+        middle.style.cursor = "grabbing";
+    });
+
+    middle.addEventListener("pointerup", (e) => {
+        middle.style.cursor = "grab";
+    });
+
+    middle.addEventListener("pointermove", (e) => {
+        if (!isPointerDown === true) {
+            return;
+        }
+        e.preventDefault();
+        carousel.style.left = `${e.offsetX - pointerX}px`;
+    });
+
+    window.addEventListener("pointerup", () => {
+        isPointerDown = false;
+    });
+
+    middle.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+    });
 };
