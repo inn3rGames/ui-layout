90fe0a49e0a5556ca2d16a2e12b2643c45e892f1
inn3rGames <hi@inn3r.com>
2022-03-22 06:39:15 +0200

    Add multiple cards

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 window.onload = () => {
-    let sparks = Array.from(document.getElementsByClassName("spark"));
-    let enabled = "spark on";
-    let disabled = "spark off";
+    let cards = Array.from(document.getElementsByClassName("item-card"));
 
-    function enableSparks(sparkIndex) {
-        for (let i = 0; i < sparkIndex + 1; i++) {
-            let spark = sparks[i];
-            spark.className = enabled;
-        }
-    }
+    for (let i = 0; i < cards.length; i++) {
 
-    function disableSparks(sparkIndex) {
-        for (let i = sparks.length - 1; i >= sparkIndex; i--) {
-            let spark = sparks[i];
-            spark.className = disabled;
-        }
-    }
+        let sparks = Array.from(cards[i].getElementsByClassName("spark"));
+        let enabled = "spark on";
+        let disabled = "spark off";
 
-    for (let i = 0; i < sparks.length; i++) {
-        let spark = sparks[i];
-        let sparkIndex = sparks.indexOf(spark);
-        spark.addEventListener("pointerdown", () => {
-            if (spark.className === disabled) {
-                enableSparks(sparkIndex);
-            } else {
-                disableSparks(sparkIndex);
+        function enableSparks(sparkIndex) {
+            for (let i = 0; i < sparkIndex + 1; i++) {
+                let spark = sparks[i];
+                spark.className = enabled;
             }
-        });
+        }
+
+        function disableSparks(sparkIndex) {
+            for (let i = sparks.length - 1; i >= sparkIndex; i--) {
+                let spark = sparks[i];
+                spark.className = disabled;
+            }
+        }
+
+        for (let i = 0; i < sparks.length; i++) {
+            let spark = sparks[i];
+            let sparkIndex = sparks.indexOf(spark);
+            spark.addEventListener("pointerdown", () => {
+                if (spark.className === disabled) {
+                    enableSparks(sparkIndex);
+                } else {
+                    disableSparks(sparkIndex);
+                }
+            });
+        }
     }
 };
