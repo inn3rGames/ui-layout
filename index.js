048f0ec30849589a1d1549472e2c7e3aef39510a
inn3rGames <hi@inn3r.com>
2022-03-23 09:57:59 +0200

    Fix item cards

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 window.onload = () => {
     let cards = Array.from(document.getElementsByClassName("item-card"));
+    let images = Array.from(document.getElementsByClassName("item-image"));
+
+    let imagesLocations = [
+        "url(./assets/images/SampleItem-b.png)",
+        "url(./assets/images/SampleItem-c.png)",
+        "url(./assets/images/SampleItem-d.png)",
+        "url(./assets/images/SampleItem.png)",
+    ];
 
     for (let i = 0; i < cards.length; i++) {
+        images[i].style.backgroundImage = imagesLocations[i];
+
         let sparks = Array.from(cards[i].getElementsByClassName("spark"));
         let enabled = "spark on";
         let disabled = "spark off";
@@ -35,12 +45,15 @@ window.onload = () => {
 
     let container = document.getElementById("carousel-container");
     let carousel = document.getElementById("carousel");
+
     let isPointerDown = false;
-    let pointerX;
+    let startX;
+    let carouselStartX;
 
     container.addEventListener("pointerdown", (e) => {
         isPointerDown = true;
-        pointerX = e.clientX - carousel.offsetLeft;
+        startX = e.clientX;
+        carouselStartX = carousel.offsetLeft;
         container.style.cursor = "grabbing";
     });
 
@@ -51,16 +64,18 @@ window.onload = () => {
     container.addEventListener("pointerup", (e) => {
         isPointerDown = false;
         container.style.cursor = "grab";
+        console.log(`Moved ${carousel.offsetLeft - carouselStartX}px`);
     });
 
     container.addEventListener("pointermove", (e) => {
-        if (!isPointerDown === true) {
-            return;
-        }
         e.preventDefault();
 
-        let distance = pointerX - e.clientX;
-        carousel.style.left = `${-distance}px`;
+        if (isPointerDown === true) {
+            let distance = startX - e.clientX;
+            carousel.style.left = `${carouselStartX - distance}px`;
+        } else {
+            return;
+        }
     });
 
     container.addEventListener("contextmenu", (e) => {
